e8b8ef0abac189e88a076ecde25baef2065371b6
nutnarinnn <nutnarinjel@gmail.com>
2024-08-09 11:58:47 +0700

    Add input fields to LoginForm & SignUpForm

--- travelmate/pages/components/LoginForm.js
+++ travelmate/pages/components/LoginForm.js
@@ -1,29 +1,83 @@
 import { signIn } from "next-auth/react";
 import { useState } from "react";
 import { FcGoogle } from "react-icons/fc";
+import { FaRegEye, FaRegEyeSlash } from "react-icons/fa";
 import SignUpForm from "./SignUpForm";
 
 export default function LoginForm() {
-    const [showSignUp, setShowSignUp] = useState(false);
+  const [showSignUp, setShowSignUp] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
 
   const toggleSignUpForms = () => {
     setShowSignUp(!showSignUp);
   };
+
+  const handleLogin = async () => {
+    await signIn("credentials", {
+      email,
+      password,
+      callbackUrl: "http://localhost:3000",
+    });
+  };
+
   return (
     <div>
       {showSignUp ? (
         <SignUpForm toggleForms={toggleSignUpForms} />
       ) : (
         <div>
-          <div
-            onClick={() =>
-              signIn("google", { callbackUrl: "http://localhost:3000" })
-            }
-            className="flex mb-2 my-4 w-60 justify-center items-center gap-3 border border-gray-300 p-2 rounded-md group cursor-pointer hover:shadow-md m-auto"
-          >
-            <FcGoogle className="text-2xl" />
-            <span className="text-base">Continue with Google</span>
+          <div className="flex text-2xl justify-center mb-4 font-bold">
+            Log in
           </div>
+
+          <div className="flex flex-col mb-4">
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="border border-gray-300 p-2 rounded-md"
+              placeholder="Email"
+            />
+          </div>
+
+          <div className="flex flex-col mb-2 relative">
+            <input
+              type={showPassword ? "text" : "password"}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="border border-gray-300 p-2 rounded-md"
+              placeholder="Password"
+            />
+            <button
+              type="button"
+              className="absolute inset-y-0 right-0 flex items-center px-3 text-gray-400"
+              onMouseDown={() => setShowPassword(true)}
+              onMouseUp={() => setShowPassword(false)}
+            >
+              {showPassword ? <FaRegEye /> : <FaRegEyeSlash />}
+            </button>
+          </div>
+
+          <div className="flex text-xs justify-center mb-1">
+            <button
+              // onClick=
+              className="text-blue-500 ml-1"
+            >
+              Forgot password?
+            </button>
+          </div>
+
+          <div
+            onClick={handleLogin}
+            className="flex mb-2 justify-center items-center gap-3 border bg-gray-900 hover:border-gray-200 hover:bg-gray-800 p-2 rounded-md group cursor-pointer hover:shadow-lg m-auto mt-2"
+          >
+            <span className="text-base text-white group-hover:text-white font-semibold">
+              Log in
+            </span>
+          </div>
+
           <div className="flex text-xs justify-center">
             No account?
             <button
@@ -32,6 +86,23 @@ export default function LoginForm() {
             >
               Create one
             </button>
+          </div>
+
+          {/* Separator */}
+          <div className="flex items-center my-4">
+            <div className="flex-grow border-t border-gray-300"></div>
+            <span className="mx-2 text-gray-500 text-sm">Or</span>
+            <div className="flex-grow border-t border-gray-300"></div>
+          </div>
+
+          <div
+            onClick={() =>
+              signIn("google", { callbackUrl: "http://localhost:3000" })
+            }
+            className="flex mb-2 my-4 w-80 justify-center items-center gap-3 border border-gray-300 p-2 rounded-md group cursor-pointer hover:shadow-md m-auto"
+          >
+            <FcGoogle className="text-2xl" />
+            <span className="text-base">Continue with Google</span>
           </div>
         </div>
       )}
--- travelmate/pages/components/SignUpForm.js
+++ travelmate/pages/components/SignUpForm.js
@@ -1,13 +1,27 @@
 import { signIn } from "next-auth/react";
 import { FcGoogle } from "react-icons/fc";
 import { useState } from "react";
+import { FaRegEye, FaRegEyeSlash } from "react-icons/fa";
 import LoginForm from "./LoginForm";
 
 export default function SignUpForm() {
   const [showLogin, setShowLogin] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
 
   const toggleLoginForms = () => {
     setShowLogin(!showLogin);
+  };
+
+  const handleSignUp = async () => {
+    await signIn("credentials", {
+      email,
+      password,
+      callbackUrl: "http://localhost:3000",
+    });
   };
 
   return (
@@ -16,15 +30,68 @@ export default function SignUpForm() {
         <LoginForm toggleForms={toggleLoginForms} />
       ) : (
         <div>
-          <div
-            onClick={() =>
-              signIn("google", { callbackUrl: "http://localhost:3000" })
-            }
-            className="flex mb-2 my-4 w-60 justify-center items-center gap-3 border border-gray-300 p-2 rounded-md group cursor-pointer hover:shadow-md m-auto"
-          >
-            <FcGoogle className="text-2xl" />
-            <span className="text-base">Continue with Google</span>
+          <div className="flex text-2xl justify-center mb-4 font-bold">
+            Sign up
           </div>
+
+          <div className="flex flex-row mb-4 gap-2">
+            <div className="flex-grow w-1">
+              <input
+                type="text"
+                value={firstname}
+                onChange={(e) => setFirstname(e.target.value)}
+                className="border border-gray-300 p-2 rounded-md w-full"
+                placeholder="Firstname"
+              />
+            </div>
+            <div className="flex-grow w-1">
+              <input
+                type="text"
+                value={lastname}
+                onChange={(e) => setLastname(e.target.value)}
+                className="border border-gray-300 p-2 rounded-md w-full"
+                placeholder="Lastname"
+              />
+            </div>
+          </div>
+
+          <div className="flex flex-col mb-4">
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="border border-gray-300 p-2 rounded-md"
+              placeholder="Email"
+            />
+          </div>
+
+          <div className="flex flex-col mb-4 relative">
+            <input
+              type={showPassword ? "text" : "password"}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="border border-gray-300 p-2 rounded-md"
+              placeholder="Password"
+            />
+            <button
+              type="button"
+              className="absolute inset-y-0 right-0 flex items-center px-3 text-gray-400"
+              onMouseDown={() => setShowPassword(true)}
+              onMouseUp={() => setShowPassword(false)}
+            >
+              {showPassword ? <FaRegEye /> : <FaRegEyeSlash />}
+            </button>
+          </div>
+
+          <div
+            onClick={handleSignUp}
+            className="flex mb-2 justify-center items-center gap-3 border bg-gray-900 hover:border-gray-200 hover:bg-gray-800 p-2 rounded-md group cursor-pointer hover:shadow-lg m-auto mt-2"
+          >
+            <span className="text-base text-white group-hover:text-white font-semibold">
+              Sign up
+            </span>
+          </div>
+
           <div className="flex text-xs justify-center">
             Already have an account?
             <button
@@ -33,6 +100,23 @@ export default function SignUpForm() {
             >
               Log in
             </button>
+          </div>
+
+          {/* Separator */}
+          <div className="flex items-center my-4">
+            <div className="flex-grow border-t border-gray-300"></div>
+            <span className="mx-2 text-gray-500 text-sm">Or</span>
+            <div className="flex-grow border-t border-gray-300"></div>
+          </div>
+
+          <div
+            onClick={() =>
+              signIn("google", { callbackUrl: "http://localhost:3000" })
+            }
+            className="flex mb-2 my-4 w-80 justify-center items-center gap-3 border border-gray-300 p-2 rounded-md group cursor-pointer hover:shadow-md m-auto"
+          >
+            <FcGoogle className="text-2xl" />
+            <span className="text-base">Continue with Google</span>
           </div>
         </div>
       )}
